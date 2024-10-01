2d3a3893d546de5b8f0e8b397733a76acbaae4f8
vicentelyrio <vicentelyrio@gmail.com>
2024-09-30 22:27:46 -0300

    updating mason

--- lua/plugins/code/mason.lua
+++ lua/plugins/code/mason.lua
@@ -18,7 +18,7 @@ return {
       'html',
       'jsonls',
       'lua_ls',
-      'tsserver',
+      'ts_ls',
     }
 
     mason.setup()
@@ -34,19 +34,19 @@ return {
       end,
     }
 
-    -- [[ Load individual languages setup ]]
-    local opts = {}
-
-    for _, server in pairs(servers) do
-      server = vim.split(server, '@')[1]
-
-      local require_ok, conf_opts = pcall(require, 'plugins.code.languages.' .. server)
-
-      if require_ok then
-        opts = vim.tbl_deep_extend('force', conf_opts, opts)
-      end
-
-      lspconfig[server].setup(opts)
-    end
+    -- -- [[ Load individual languages setup ]]
+    -- local opts = {}
+    --
+    -- for _, server in pairs(servers) do
+    --   server = vim.split(server, '@')[1]
+    --
+    --   local require_ok, conf_opts = pcall(require, 'plugins.code.languages.' .. server)
+    --
+    --   if require_ok then
+    --     opts = vim.tbl_deep_extend('force', conf_opts, opts)
+    --   end
+    --
+    --   lspconfig[server].setup(opts)
+    -- end
   end
 }
