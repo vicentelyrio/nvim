ff9795839853689756c1317b01727c2f8517f084
vicentelyrio <vicentelyrio@gmail.com>
2024-10-13 19:26:02 -0300

    update pio lib

--- lua/plugins/code/vimpio.lua
+++ lua/plugins/code/vimpio.lua
@@ -1,5 +1,8 @@
 return {
-  'normen/vim-pio',
-  lazy=false,
+  'anurag3301/nvim-platformio.lua',
+  dependencies = {
+    { 'akinsho/nvim-toggleterm.lua' },
+    { 'nvim-telescope/telescope.nvim' },
+    { 'nvim-lua/plenary.nvim' },
+  },
 }
-
