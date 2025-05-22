9c12e176300ab9072ebe230fd5d76595e633119f
vicentelyrio <vicentelyrio@gmail.com>
2025-05-22 19:42:56 -0300

    dotenv file detection

--- lua/settings/config.lua
+++ lua/settings/config.lua
@@ -110,6 +110,17 @@ local disabled_built_ins = {
 for _, plugin in pairs(disabled_built_ins) do
   vim.g['loaded_' .. plugin] = 1
 end
+
+vim.filetype.add({
+  pattern = {
+    ['%.env%.%w+'] = 'sh',  -- Matches .env.anything
+    ['%.env$'] = 'sh',      -- Standard .env files
+  },
+  filename = {
+    ['.env.stg'] = 'sh',
+    ['.env.prd'] = 'sh',
+    ['.env.dev'] = 'sh',
+    ['.env.local'] = 'sh',
+  }
+})
 -- }}}
-
-
